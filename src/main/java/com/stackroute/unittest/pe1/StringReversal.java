443af6c7b210e86a12fd9f8681cdd661abfb4011
imneetkaur <imneetkaur007@gmail.com>
2019-05-02 23:23:42 +0530

    Modified with test cases

--- src/main/java/com/stackroute/unittest/pe1/StringReversal.java
+++ src/main/java/com/stackroute/unittest/pe1/StringReversal.java
@@ -1,4 +1,14 @@
 package com.stackroute.unittest.pe1;
 
 public class StringReversal {
+
+    public String reverseString(String str){
+        String reverseString="";
+
+        for (int i = str.length()-1; i >= 0; i-- ) {
+            reverseString = reverseString + str.charAt(i);
+        }
+        System.out.println(reverseString);
+        return reverseString;
+    }
 }
--- src/main/test/com/stackroute/unittest/pe1/StringReversalTest.java
+++ src/main/test/com/stackroute/unittest/pe1/StringReversalTest.java
@@ -2,16 +2,27 @@ package com.stackroute.unittest.pe1;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class StringReversalTest {
+    StringReversal objReverse;
 
     @Before
     public void setUp() throws Exception {
+        objReverse=new StringReversal();
     }
 
     @After
     public void tearDown() throws Exception {
+        objReverse=null;
+    }
+
+    @Test
+    public void testForValidInput(){
+        String expectedValue="teenmi";
+        String actualValue=objReverse.reverseString("imneet");
+        assertEquals(expectedValue,actualValue);
     }
 }
